f5a5049184e00a5c0e506ed48397dbd6cd0c2e07
mcvmcv <mcveagh@gmail.com>
2022-01-21 15:13:53 +1300

    Initial commit

--- src/GenieTables.jl
+++ src/GenieTables.jl
@@ -1,5 +1,59 @@
 module GenieTables
 
-greet() = print("Hello World!")
 
-end # module
+export AbstractColumn, AbstractTable
+
+
+
+abstract type AbstractColumn end
+
+function render_header(column::AbstractColumn, object::AbstractModel) end
+
+function render_row(column::AbstractColumn, object::AbstractModel) end
+
+
+
+
+struct DeleteColumn <: AbstractColumn
+    header_template = ""
+    row_template = ""
+end
+
+
+
+
+
+
+
+abstract type AbstractTable end
+
+
+
+
+
+
+
+
+
+struct AdminTable{T} <: AbstractTable where T 
+    objects::T
+    columns::Vector{<:AbstractColumn}
+end
+
+
+function install(dest::String; force = false) :: Nothing
+    src = abspath(normpath(joinpath(@__DIR__, "..", Genie.Plugins.FILES_FOLDER)))
+    
+    for f in readdir(src)
+        isfile(f) && continue
+        isdir(f) || mkpath(joinpath(src, f))
+        
+        Genie.Plugins.install(joinpath(src, f), dest, force = force)
+    end
+    
+    nothing
+end
+
+
+
+end
